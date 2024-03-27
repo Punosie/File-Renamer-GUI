1e7cb160c6f1c8de9b439785b95547654a863c68
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-27 21:21:33 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+import re
+
+def rename_file(root_folder_path, regex_pattern):
+    print("Called")
+    root_folder_path = Path(root_folder_path)
+    print(f'{root_folder_path}')
+    renamed = False
+    
+    for folder_name, _, files in os.walk(root_folder_path):
+        for file_name in files:
+            file_name_without_extension = Path(file_name).stem
+            match = re.search(regex_pattern, file_name_without_extension)
+            if match:
+                episode_number = match.group(1)
+                new_file_name = f"Episode {episode_number}"
+                old_file_path = Path(folder_name) / file_name
+                new_file_path = Path(folder_name) / (new_file_name + old_file_path.suffix)
+                if old_file_path != new_file_path:
+                    old_file_path.rename(new_file_path)
+                    renamed = True
+
+    if renamed:
+        print("Files renamed")
+        return 'Renaming process completed.'
+    else:
+        print("Files not renamed")
+        return 'Files are already renamed.'
---
+++ gui.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from app import rename_file
+
+
+root = tk.Tk()
+root.geometry('500x500')
+root.title("File Renamer")
+
+
+def open_folder_dialog():
+    folder_path = filedialog.askdirectory(title="Select a Folder")
+    if folder_path:
+        address_entry.delete(0, tk.END)
+        address_entry.insert(0, folder_path)
+
+
+def on_button_click():
+    address_in = address_entry.get()
+    reg_in = reg_entry.get()
+    # reg_in = escape(reg_in)
+    result_message = rename_file(address_in, reg_in)
+    messagebox.showinfo("Result", result_message)
+    print("Folder Dir", address_in)
+    print("Regex: ", reg_in)
+    print(result_message)
+
+# Label
+address_label = tk.Label(text='Folder Dir: ', font=('Helvetica', 12))
+reg_label = tk.Label(text='Regex: ', font=('Helvetica', 12))
+
+# Entry
+address_entry = tk.Entry(root, width=30, font=('Helvetica', 12))
+reg_entry = tk.Entry(root, width=30, font=('Helvetica', 12))
+
+# Button 
+add_button = tk.Button(root, text="Open Folder", command=open_folder_dialog, font=('Helvetica', 12))
+ok_button = tk.Button(root, text="OK", command=on_button_click, font=('Helvetica', 12))
+
+# Packing
+
+address_label.grid(row=0, column=0, pady=10)
+address_entry.grid(row=0, column=1, pady=10)
+
+add_button.grid(row=1, column=1, pady=10)
+
+reg_label.grid(row=2, column=0, pady=10)
+reg_entry.grid(row=2, column=1, pady=10)
+
+ok_button.grid(row=3, column=1, pady=10)
+
+
+root.mainloop()
